f36976346c35784489b7fdf53a208201280c72c2
Yonatan Israeli <yonirisraeli@gmail.com>
2024-08-21 20:29:07 -0400

    Update Wi-Fi Profile Viewer.bat

--- Wi-Fi Profile Viewer.bat
+++ Wi-Fi Profile Viewer.bat
@@ -2,7 +2,7 @@
 setlocal
 title Wi-Fi Profile Viewer
 echo Program Name: Wi-Fi Profile Viewer
-echo Version: 1.1.4
+echo Version: 1.1.5
 echo Developer: @YonatanReuvenIsraeli
 echo Website: https://www.yonatanreuvenisraeli.dev
 echo License: GNU General Public License v3.0
@@ -10,17 +10,23 @@ goto "Start"
 
 :"Start"
 netsh wlan show profile
+if not "%errorlevel%"=="0" goto "Error1"
 goto "Profile"
+
+:"Error1"
+echo There has been an error! Press any key to try again.
+pause > nul 2>&1
+goto "Start"
 
 :"Profile"
 echo.
 set Profile=
 set /p Profile="Which Wi-Fi profile do you want to view? "
 netsh wlan show profile name="%Profile%" key=clear
-if not "%errorlevel%"=="0" goto "Error"
+if not "%errorlevel%"=="0" goto "Error2"
 goto "Another"
 
-:"Error"
+:"Error2"
 echo Profile %Profile% does not exist! Pleas try again.
 goto "Profile"
 
